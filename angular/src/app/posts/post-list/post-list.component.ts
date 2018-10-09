7d5f5595b4ada187ebadc194049659a7afbeddd0
Sanjeev Kumar <sanjeevkumar@smartdatainc.net>
2018-10-09 18:35:55 +0530

    all

---
+++ angular/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+  posts = [ 
+     {title:'first title',content:'this is first content' },
+     {title:'secound title',content:'this is sec content' },
+     {title:'third title',content:'this is third content' },
+  ] 
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
